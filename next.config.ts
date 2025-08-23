95a1697348f9f1e0ccdc4d0847b2011da2e4ae46
hermansh-id <hermansh.id@gmail.com>
2025-08-24 02:27:46 +0700

    fix: build ws libsql

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  outputFileTracingIncludes: {
+    './/': [
+      './node_modules/@libsql/darwin/**/*',
+      './node_modules/@libsql/linux/**/*',
+    ],
+  },
   serverExternalPackages: ["@libsql/isomorphic-ws", "ws"],
 };
 
